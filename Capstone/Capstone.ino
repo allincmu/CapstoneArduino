2b504d433dd32e27819ca0f73d9beee8bf1b759c
allincmu <58177218+allincmu@users.noreply.github.com>
2022-04-11 11:12:51 -0400

    Update Capstone.ino

--- Capstone/Capstone.ino
+++ Capstone/Capstone.ino
@@ -1,38 +1,15 @@
-/*****************************************************************************/
-//  Function:  Get the accelemeter of the x/y/z axis. 
-//  Hardware:  Grove - 3-Axis Analog Accelerometer
-//  Arduino IDE: Arduino-1.0
-//  Author:  Frankie.Chu    
-//  Date:    Jan 11,2013
-//  Version: v1.0
-//  by www.seeedstudio.com
-//
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
-/*******************************************************************************/
+
 
 #include "ADXL335.h"
 ADXL335 accelerometer;
 
-#include <SoftwareSerial.h>                                                    //add the soft serial libray
-#define rxpin 11                                                                //set the RX pin to pin 11
-#define txpin 10                                                               //set the TX pin to pin 10
-SoftwareSerial BTSerial(rxpin, txpin);      
+#include <SoftwareSerial.h>                                  //add the soft serial libray
+#define rxpin 11                                             //set the RX pin to pin 11
+#define txpin 10                                             //set the TX pin to pin 10
+SoftwareSerial BTSerial(txpin, rxpin);      
 
 String inputString = "";         // a String to hold incoming data
-bool stringComplete = false;  // whether the string is complete
+bool stringComplete = false;     // whether the string is complete
 
 
 
@@ -64,24 +41,24 @@ void setup()
 void loop()
 {
   
-  while (!started) {
-    if (stringComplete) {
-      Serial.println("rcvd something");
-
-      if (inputString == "start\n") {
-        Serial.println("rcvd start");
-        started = 1;
-      }
-      
-      // clear the string:
-      inputString = "";
-      stringComplete = false;
-    } else {
-      Serial.println("Waiting for start signal");
-      delay(1000);
-      serialEvent();
-    }
-  }
+//  while (!started) {
+//    if (stringComplete) {
+//      Serial.println("rcvd something");
+//
+//      if (inputString == "start\n") {
+//        Serial.println("rcvd start");
+//        started = 1;
+//      }
+//      
+//      // clear the string:
+//      inputString = "";
+//      stringComplete = false;
+//    } else {
+//      Serial.println("Waiting for start signal");
+//      delay(1000);
+//      serialEvent();
+//    }
+//  }
 
   // print the string when a newline arrives:
   if (stringComplete) {
@@ -98,19 +75,22 @@ void loop()
     stringComplete = false;
   }
 
-  
-    
-
  
-  if (count == DataAggregationInterval) {
-    count = 0;
-    BTSerial.println("Acceleration: 15, HR: 65");
-  }
+ 
 
 
-  
+  Serial.println(count);
   double magnitude;
   magnitude = get_mag_accel();
+   if (count == DataAggregationInterval) {
+    count = 0;
+    BTSerial.print(magnitude);
+    BTSerial.println(",65\n");
+    Serial.print("Send over bluetooth: ");
+    Serial.print(magnitude);
+    Serial.println(",65\n");
+  }
+
   
   delay(1000);
   count++;
@@ -163,9 +143,9 @@ double get_mag_accel()
   delay response. Multiple bytes of data may be available.
 */
 void serialEvent() {
-  while (Serial.available()) {
+  while (BTSerial.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read();
+    char inChar = (char)BTSerial.read();
     // add it to the inputString:
     inputString += inChar;
     // if the incoming character is a newline, set a flag so the main loop can
